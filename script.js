6f3706fa4e66691f73862b3d83a982b98583b37d
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2024-12-21 23:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {  
+    // Prevent default form submission behavior  
+    event.preventDefault();  
 
+    // Get the form elements  
+    const firstName = document.forms['form1'].elements['fname'].value;  
+    const lastName = document.forms['form1'].elements['lname'].value;  
+
+    // Create the full name string  
+    const fullName = `${firstName} ${lastName}`;  
+
+    // Display the full name in an alert  
+    alert(fullName);  
 }
